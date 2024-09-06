63a3ff1045fa80d1f825606b3d6bf5efad54f6bf
tjsp9830 <95325682+tjsp9830@users.noreply.github.com>
2024-09-07 06:17:54 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 16.82 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.cs
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.cs
@@ -9,19 +9,20 @@ public class Example
         Console.Clear();
         s = Console.ReadLine();
 
+        
         string res = ""; // 비어있는 string변수를 만들어주고
 
         for(int i=0; i < s.Length;i++)
         {
             char c = s[i]; //string의 i번째 요소를 char에 담음
-            
-            if(c >= 65 && c <=90) //그 char이 아스키코드의 대문자라면
-                res += (char)(c + 32); //그 char은 int로 계산 가능하니, 32를 더해줘서 소문자로 만듦
+
+            //char은 int로 계산 가능하니, 그 char이 아스키코드의 대문자인지 체크
+            if(c >= 65 && c <=90) 
+                res += (char)(c + 32); //맞다면 32를 더해줘서 소문자로 만들고, string변수에 한글자씩 담아줌
             else
-                res += (char)(c - 32); //else의 경우 소문자일테니 32를 빼서 대문자로 만듦
+                res += (char)(c - 32); //else의 경우 소문자일테니 32를 빼서 대문자로 만들고, string변수에 한글자씩 담아줌
         }
 
         Console.Write(res); //Write()로 공백없이 한글자씩 출력해줌
-        
     }
 }
