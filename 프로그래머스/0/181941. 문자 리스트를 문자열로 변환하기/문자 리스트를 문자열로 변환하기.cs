1c2ba281bc6d899657a13933422f8032cc72f660
tjsp9830 <95325682+tjsp9830@users.noreply.github.com>
2025-02-10 07:35:37 +0900

    [level 0] Title: 문자 리스트를 문자열로 변환하기, Time: 0.21 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public string solution(string[] arr) {
+        string answer = "";
+        
+   for (int i = 0; i < arr.Length; i++)
+    {
+        answer += arr[i];
+    }
+        
+        return answer;
+    }
+}
