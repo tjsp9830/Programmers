56828d7321cc2db50aad8001bb421462548282f1
tjsp9830 <95325682+tjsp9830@users.noreply.github.com>
2025-02-10 14:08:28 +0900

    [Bronze V] Title: 아스키 코드, Time: 48 ms, Memory: 5444 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cs
@@ -0,0 +1,22 @@
+// 
+// 
+using System;
+using System.ComponentModel.DataAnnotations;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        char chInput = input[0];
+        int num = chInput;
+        Console.WriteLine(num);
+
+
+
+    }
+
+
+
+}
+
